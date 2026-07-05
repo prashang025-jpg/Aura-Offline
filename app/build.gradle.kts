bcc99e5e544ea2a01b04d6f7111d577b3b3542a8
prashang025-jpg <prashang025@gmail.com>
2026-07-05 05:46:28 +0300

    Fix namespace and applicationId in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,11 +9,11 @@ plugins {
 }
 
 android {
-    namespace = "namespace = "com.localmind.app"
+    namespace = "com.localmind.app"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "namespace = "com.localmind.app"
+        applicationId = "com.prashan.auraoffline.app"
         minSdk = 26
         targetSdk = 35
         versionCode = 1       // VERSION: manually change karo jab update karna ho
